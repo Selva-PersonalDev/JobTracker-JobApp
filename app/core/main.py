50612d98152d320cb28076c9dd582870415eee49
Selvakumar d <Selvakumar.d@wpp.com>
2026-02-01 17:29:32 +0530

    Updated the main.py file

--- app/core/main.py
+++ app/core/main.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
 from app.core.routes import router
-from app.core.storage import download_db_from_gcs
 
 app = FastAPI(title="Job Tracker")
-
-download_db_from_gcs()
 
 app.add_middleware(SessionMiddleware, secret_key="jobtracker-secret")
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
-    
+
 app.include_router(router)
