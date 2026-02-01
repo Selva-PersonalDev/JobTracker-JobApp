e7d739a1a90cf77f6250554411a07236fae871fb
Selvakumar d <Selvakumar.d@wpp.com>
2026-02-01 17:05:15 +0530

    Updated the DB file

--- app/core/db.py
+++ app/core/db.py
@@ -1,15 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.storage import download_db_from_gcs
 
-DB_PATH = "/tmp/jobs.db"
+DB_PATH = os.getenv("DB_PATH", "/data/jobs.db")
 
-download_db_from_gcs()
-
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
+DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
